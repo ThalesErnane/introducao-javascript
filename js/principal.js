239ef34860e93c187e5a56a058dcd8d50adcb764
Thales Ernane de Souza <talesernani@gmail.com>
2020-04-08 21:12:03 -0300

    adicionando função no js

--- js/principal.js
+++ js/principal.js
@@ -56,9 +56,63 @@ if ( alturaEhValido && pesoEhValido ){
 }
 */
 }
+// alert(tdImc.textContent);    
 
 
 
-// alert(tdImc.textContent);
+function mostraMensagem( ){
+    console.log("Olá eu fui clicado !");
+};
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    // pega os valores da tela 
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    // cria as td 
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td"); 
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    // preenche os valores na td
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // adicionando na tr
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
 
 
+    // pegando o elemento, tabela ()
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr); // adiciona na tabela 
+
+
+    /*
+    console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
+    */
+
+});
+
+
+console.log(botaoAdicionar);
